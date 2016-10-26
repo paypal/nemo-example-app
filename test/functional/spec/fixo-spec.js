ad2ded28b1a617d134ba1493e6baf256ee9c1464
Matt Edelman <medelman@paypal.com>
2016-10-26 07:33:43 -0700

    some changes for fixo experimentation

--- test/functional/spec/fixo-spec.js
+++ test/functional/spec/fixo-spec.js
@@ -3,23 +3,9 @@ var Nemo = require('nemo');
 var nemo = {};
 var util = require('../util');
 
-describe('@generic@', function () {
+describe('@fixo@', function () {
     before(function (done) {
-        nemo = Nemo(function (err) {
-            <<<<<<< c209efe7d01d3ebd51b35c1eb10b7fb66db1eee3
-            if (err) {
-                return done(err);
-            }
-            =======
-                //nemo.wd.logging.installConsoleHandler();
-                //nemo.wd.logging.getLogger().setLevel(nemo.wd.logging.Level.ERROR);
-                nemo.driver.controlFlow().on('uncaughtException', function (err) {
-                    console.error('err', err);
-                    throw err;
-                });
-            >>>>>>> wdb stuff
-            done();
-        });
+        nemo = Nemo(done);
     });
     after(function (done) {
         nemo.driver.quit().then(done);
@@ -28,64 +14,7 @@ describe('@generic@', function () {
         //login
         nemo.driver.get(nemo.data.baseUrl);
         util.waitForJSReady(nemo);
-        nemo.view._waitVisible('id:email').sendKeys('me@mine.com');
-        nemo.view._find('#password').sendKeys('11111111');
-        nemo.view._find('#loginbutton').click();
-        //add card
-        nemo.view._waitVisible('#cc').sendKeys('123456789012');
-        nemo.view._finds('#type option').then(function(opts) {
-            return opts[1].click();
-        });
-        nemo.view._find('#ccbutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //add bank
-        nemo.view._find('#addbalink').click();
-        nemo.view._waitVisible('#ban').sendKeys('0123545332');
-        nemo.view._find('#brn').sendKeys('343434');
-        nemo.view._find('#babutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //logout
-        nemo.view._find('#logoutlink').click();
-        util.waitForJSReady(nemo);
 
-        nemo.view._waitVisible('id:email').sendKeys('me@mine.com');
-        nemo.view._find('#password').sendKeys('11111111');
-        nemo.view._find('#loginbutton').click();
-        //add card
-        nemo.view._waitVisible('#cc').sendKeys('123456789012');
-        nemo.view._finds('#type option').then(function(opts) {
-            return opts[1].click();
-        });
-        nemo.view._find('#ccbutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //add bank
-        nemo.view._find('#addbalink').click();
-        nemo.view._waitVisible('#ban').sendKeys('0123545332');
-        nemo.view._find('#brn').sendKeys('343434');
-        nemo.view._find('#babutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //logout
-        nemo.view._find('#logoutlink').click();
-        util.waitForJSReady(nemo);
-
-        nemo.view._waitVisible('id:email').sendKeys('me@mine.com');
-        nemo.view._find('#password').sendKeys('11111111');
-        nemo.view._find('#loginbutton').click();
-        //add card
-        nemo.view._waitVisible('#cc').sendKeys('123456789012');
-        nemo.view._finds('#type option').then(function(opts) {
-            return opts[1].click();
-        });
-        nemo.view._find('#ccbutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //add bank
-        nemo.view._find('#addbalink').click();
-        nemo.view._waitVisible('#ban').sendKeys('0123545332');
-        nemo.view._find('#brn').sendKeys('343434');
-        nemo.view._find('#babutton').click();
-        nemo.view._waitVisible('p.result.good');
-        //logout
-        nemo.view._find('#logoutlink').click();
         nemo.view._waitVisible('#email').then(function () {
             done();
         }, function (err) {
