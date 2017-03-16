23eeeb46e39152bb929d0b034608221b03e1cad0
Matt Edelman <medelman@paypal.com>
2017-03-15 20:30:53 -0700

    first version of nemo-3-alpha

---
+++ test/functional/spec/async-spec.js
@@ -0,0 +1,23 @@
+/*global nemo:true, describe:true, it:true */
+let Nemo = require('nemo');
+let nemo = {};
+const util = require('../util');
+
+describe('@async@ uses ES6 async/await', function () {
+    before(function () {
+        return Nemo().then(function (n) {
+            nemo = n;
+        });
+    });
+    after(function () {
+        return nemo.driver.quit();
+    });
+    it('should reject a promise', async function () {
+        //login
+        await nemo.driver.get('http://www.google.com');
+        await nemo.view._waitVisible('[name=q]');
+        await nemo.view._find('[name=q]').sendKeys('webdriver');
+        await nemo.view._find('[name=btnG]').click();
+        return nemo.driver.wait(nemo.wd.until.titleIs('webdriver - Google Search'), 1000);
+    });
+});
