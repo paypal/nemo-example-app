abece649c64f7849961bc82067bb90fd966e6b3d
Matt Edelman <medelman@paypal.com>
2017-03-28 17:54:22 -0700

    adding stuff for nemo-runner

--- test/functional/spec/gsync-spec.js
+++ test/functional/spec/gsync-spec.js
@@ -1,16 +1,8 @@
 /*global nemo:true, describe:true, it:true */
 const util = require('../util');
 
-describe('@async@ uses ES6 async/await', function () {
-    it('should kill the driver', async function () {
-        //google search
-        let nemo = this.nemo;
-        await nemo.driver.get('http://www.google.com');
-        await nemo.view._waitVisible('[name=qz]');
-        await nemo.view._find('[name=q]').sendKeys('webdriver');
-        await nemo.vie._find('[name=btnG]').click();
-        return nemo.driver.wait(nemo.wd.until.titleIs('webdriver - Google Search'), 1000);
-    });
+describe('@gsync@ uses ES6 async/await', function () {
+
     it('should use async/await control flow', async function () {
         //google search
         let nemo = this.nemo;
